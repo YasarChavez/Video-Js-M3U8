ffbddfb76762c56e6c3e0228b42b3e9d7adfc67e
YasarChavez <chavezyasar@gmail.com>
2024-02-28 13:40:56 -0300

    first commit

---
+++ Test Links.md
@@ -0,0 +1,3 @@
+https://s3.amazonaws.com/sffs-upload-steve/v1/hls/sffs-2014-short-012/sffs-2014-short-012.m3u8
+
+https://bitmovin-a.akamaihd.net/content/playhouse-vr/m3u8s/105560.m3u8
---
+++ script.js
@@ -0,0 +1,5 @@
+window.onload = function () {
+    // Configuración de Video.js
+    var player = videojs("my-video");
+    player.play(); // Auto-reproducir el video al cargar la página
+};
